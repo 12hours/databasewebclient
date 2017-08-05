44b8fbc95eb64b21581a370c1c7995b0e7fb023c
nm <mon-keigh@ya.ru>
2017-08-05 08:05:12 +0000

    rest service base changed to /api

--- src/main/java/com/example/demo/SurveyDemoApplication.java
+++ src/main/java/com/example/demo/SurveyDemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SurveyDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SurveyDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SurveyDemoApplication.class, args);
+    }
 }
