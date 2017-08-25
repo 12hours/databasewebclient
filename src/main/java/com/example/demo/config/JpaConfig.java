c591f04931c46fbcec20cf4afd73be170d186d74
nm <mon-keigh@ya.ru>
2017-08-25 11:37:47 +0000

    add logging for database operations

--- src/main/java/com/example/demo/config/JpaConfig.java
+++ src/main/java/com/example/demo/config/JpaConfig.java
@@ -1,6 +1,8 @@
 package com.example.demo.config;
 
 import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,12 +19,15 @@ import java.util.Properties;
 @EnableJpaRepositories(basePackages = "com.example.demo.dao")
 public class JpaConfig {
 
+    private static final Logger log = LoggerFactory.getLogger(DataSourceConfig.class);
+
     @Autowired
     DataSource dataSource;
 
     @Bean(name = "entityManagerFactory")
     @Profile("dev")
     public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
+        log.debug("SETTING UP HIBERNATE...");
         LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
         emf.setDataSource(dataSource);
         emf.setPackagesToScan("com.example.demo.domain");
@@ -41,6 +46,7 @@ public class JpaConfig {
     @Bean(name = "entityManagerFactory")
     @Profile("test")
     public LocalContainerEntityManagerFactoryBean entityManagerFactoryForH2(){
+        log.debug("SETTING UP HIBERNATE...");
         LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
         emf.setDataSource(dataSource);
         emf.setPackagesToScan("com.example.demo.domain");
@@ -58,6 +64,7 @@ public class JpaConfig {
     @Bean(name = "entityManagerFactory")
     @Profile("prod")
     public LocalContainerEntityManagerFactoryBean entityManagerFactoryForHSQLDB(){
+        log.debug("SETTING UP HIBERNATE...");
         //TODO
         return null;
     }
