10e6a16bf85517e54c02072d545d5ab39d34d20b
victor <victorbeliakov@gmail.com>
2017-08-27 18:54:57 +0000

    H2ServerConfiguration.java added dev profile as deault

--- src/main/java/com/example/demo/config/H2ServerConfiguration.java
+++ src/main/java/com/example/demo/config/H2ServerConfiguration.java
@@ -11,11 +11,11 @@ import java.sql.SQLException;
 
 /**
  * If you left the default port for the H2 web console at 8082 go to http://localhost:8082.
- * If you didnâ€™t change the name for the database in your properties file,
- * the default connection URL is jdbc:h2:mem:testdb and the username is â€žsaâ€œ, password empty.
+ * If you didn’t change the name for the database in your properties file,
+ * the default connection URL is jdbc:h2:mem:testdb and the username is „sa“, password empty.
  */
 @Configuration
-@Profile("test") // Only activate this in the "dev" profile
+@Profile({"test","dev"}) // Only activate this in the "dev" profile
 public class H2ServerConfiguration {
 
     // TCP port for remote connections, default 9092
