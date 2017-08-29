353889c65c9c4f6252150f5c098da163b0f65265
victorbeliakov <victorbeliakov>
2017-08-29 16:19:20 +0300

    server port now can be configured from external application.properties file

--- src/main/java/com/example/demo/SurveyDemoApplication.java
+++ src/main/java/com/example/demo/SurveyDemoApplication.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource({"classpath:application.yml","file:application.properties"})
 public class SurveyDemoApplication {
 
     public static void main(String[] args) {
